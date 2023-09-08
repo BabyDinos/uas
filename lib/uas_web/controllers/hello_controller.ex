d7f0461f9e582c6dbf97d2e4d59d67390473d14a
Marcus Lau <108362808+BabyDinos@users.noreply.github.com>
2023-09-07 20:35:37 -0400

    Request Life Cycle
    https://hexdocs.pm/phoenix/request_lifecycle.html
    Completed this section

---
+++ lib/uas_web/controllers/hello_controller.ex
@@ -0,0 +1,11 @@
+defmodule UasWeb.HelloController do
+  use UasWeb, :controller
+
+  def index(conn, _params) do
+    render(conn, :index)
+  end
+
+  def show(conn, %{"messenger" => messenger}) do
+    render(conn, :show, messenger: messenger)
+  end
+end
---
+++ lib/uas_web/controllers/hello_html.ex
@@ -0,0 +1,5 @@
+defmodule UasWeb.HelloHTML do
+  use UasWeb, :html
+
+  embed_templates "hello_html/*"
+end
