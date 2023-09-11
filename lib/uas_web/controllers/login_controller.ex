dc486d7009731a8e7b0dfa6604b59b2b98d2e288
Marcus Lau <108362808+BabyDinos@users.noreply.github.com>
2023-09-10 23:38:17 -0400

    phx.gen.auth
    Added login/register/passwords

--- lib/uas_web/controllers/login_controller.ex
+++
@@ -1,9 +0,0 @@
-defmodule UasWeb.LoginController do
-  use UasWeb, :controller
-
-  def login(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :login, layout: false)
-  end
-end
--- lib/uas_web/controllers/login_html.ex
+++
@@ -1,5 +0,0 @@
-defmodule UasWeb.LoginHTML do
-  use UasWeb, :html
-
-  embed_templates "login_html/*"
-end
